1408ff9128d4c97485a35b2ca72263bee853830b
John Puddephatt <john@jdp.org.uk>
2022-09-26 20:56:50 +0100

    move toc out of prose

--- resources/views/template-fullwidth.blade.php
+++ resources/views/template-fullwidth.blade.php
@@ -5,10 +5,12 @@
 @section('content')
       
   @include('partials.header-section', ['pattern' => 'header-opportunities'])
-    <div class="container px-4 prose !max-w-6xl">
+    <div class="container px-4">
       <div class="toc-horizontal my-12">
         {!! do_shortcode('[toc]') !!}
       </div>
+      <div class="prose !max-w-6xl">
       {{ the_content() }}     
+      </div>
   </div>
 @endsection
